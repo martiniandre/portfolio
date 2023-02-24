1aee6c4eef0e4659f8de415a1661aa6ab4614f4d
Andre Martini <andremartini.10@gmail.com>
2023-02-23 23:03:41 -0300

    create portifolio

---
+++ src/helpers/projects.ts
@@ -0,0 +1,19 @@
+export const MyProjects = [
+  {
+    name: "Remix gallery",
+    href: "https://up-gallery.vercel.app",
+  },
+  {
+    name: "Pokedex",
+    href: "https://pok3dex.vercel.app",
+  },
+  {
+    name: "Draw pixel table",
+    href: "https://pixel-table.vercel.app",
+  },
+  {
+    name: "Animated form",
+    href: "https://github.com/martiniandre/animated-form",
+  },
+
+];
